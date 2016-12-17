107213173d99817a2d340d68a538b0fdcda4e21d
WYX <wyx_msn@hotmail.com>
2016-12-17 21:46:57 +0800

    init

--- routes/about.js
+++ routes/about.js
@@ -1,3 +1,11 @@
 /**
  * Created by WYX on 2016/12/17.
  */
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('about', {title: '关于我们'});
+});
+
+module.exports = router;
