d1da2bee9a39fae38aef9fa1e37133dcdd9e109f
WYX <wyx_msn@hotmail.com>
2016-12-20 21:11:45 +0800

    transition

--- routes/artists.js
+++ routes/artists.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.render('list', {path: 'artists'});
+    res.render('list', {path: 'artists'});
+});
+
+router.get('/:id', (req, res) => {
+    res.render('artist', {id: req.params.id});
 });
 
 module.exports = router;
