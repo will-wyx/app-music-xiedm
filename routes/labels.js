f01ae6f3388e1a9ab206e8dc39be5d1a96ca815d
will <wyx_msn@hotmail.com>
2016-12-20 08:29:57 +0800

    news init

--- routes/labels.js
+++ routes/labels.js
@@ -1,8 +1,8 @@
 /**
  * Created by WYX on 2016/12/18.
  */
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('list', {path: 'labels'});
