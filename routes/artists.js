8aec7a852e39f9bc127877aab2d3ce2fb5ae2dce
WYX <wyx_msn@hotmail.com>
2016-12-24 20:31:35 +0800

    active artists&labels and dialog form

--- routes/artists.js
+++ routes/artists.js
@@ -1,9 +1,71 @@
 const express = require('express');
 const router = express.Router();
 
+const artists = [
+    {
+        title: '麦家瑜',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/1.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '王菀之',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/2.jpg',
+        code: 'oo'
+    },
+    {
+        title: '中孝介',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/3.jpg',
+        code: 'wwz'
+    },
+    {
+        title: '谢安琪',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/4.jpg',
+        code: 'xaq'
+    },
+    {
+        title: 'Leessang',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/5.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '不知名',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/6.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '黄丽玲',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/7.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '莫文蔚',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/8.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '张靓颖',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/9.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '张敬轩',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/artists/10.jpg',
+        code: 'xaq'
+    }
+];
 /* GET users listing. */
 router.get('/', (req, res) => {
-    res.render('list', {path: 'artists'});
+    res.render('list', {path: 'artists', list: artists});
 });
 
 router.get('/:id', (req, res) => {
--- routes/labels.js
+++ routes/labels.js
@@ -4,8 +4,65 @@
 const express = require('express');
 const router = express.Router();
 
+const labels = [
+    {
+        title: '华纳音乐',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/1.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '滚石唱片',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/2.png',
+        code: 'oo'
+    },
+    {
+        title: '索尼音乐',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/3.jpg',
+        code: 'wwz'
+    },
+    {
+        title: '光线传媒',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/4.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '金牌大风',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/5.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '种子音乐',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/6.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '海蝶音乐',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/7.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '华谊音乐',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/8.jpg',
+        code: 'xaq'
+    },
+    {
+        title: '乐华音乐',
+        description: '华纳音乐集团是21世纪世界三大唱片公司之一，原隶属于拥有华纳兄弟影片公司、时代杂志、财富杂志、CNN有线电视新闻网等传媒巨子的时代华纳集团，在众多行业有着卓越的成就。',
+        icon: '/images/labels/9.jpg',
+        code: 'xaq'
+    },
+];
+
 router.get('/', (req, res) => {
-    res.render('list', {path: 'labels'});
+    res.render('list', {path: 'labels', list: labels});
 });
 
 module.exports = router;
