aa8fcee68be6bcfb409920a4d13912213077f4d0
WYX <wyx_msn@hotmail.com>
2016-12-27 22:49:09 +0800

    jwysiwyg

--- routes/management/management.js
+++ routes/management/management.js
@@ -9,4 +9,24 @@ router.get('/', (req, res) => {
 router.get('/login', (req, res) => {
     res.render('management/login');
 });
+
+router.get('/news', (req, res) => {
+    res.render('management/news', {path: req.url});
+});
+router.get('/news/:id', (req, res) => {
+    res.render('management/news-edit', {path: req.url});
+});
+
+router.get('/artist', (req, res) => {
+    res.render('management/artist', {path: req.url});
+});
+router.get('/media', (req, res) => {
+    res.render('management/media', {path: req.url});
+});
+router.get('/statistics', (req, res) => {
+    res.render('management/news', {path: req.url});
+});
+router.get('/layout/:path', (req, res) => {
+    res.send(req.url);
+});
 module.exports = router;
